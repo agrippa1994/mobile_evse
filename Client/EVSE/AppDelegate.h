4c44ce92b4cbd56214a08ab421bb1c00fe7c4d21
Manuel Stampfl <091aet21@htl-kapfenberg.ac.at>
2013-12-29 01:09:16 +0100

    Repo neu initialisiert

---
+++ Client/EVSE/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EVSE
+//
+//  Created by Mani on 29.12.13.
+//  Copyright (c) 2013 Mani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Client/EVSE/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EVSE
+//
+//  Created by Mani on 29.12.13.
+//  Copyright (c) 2013 Mani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
