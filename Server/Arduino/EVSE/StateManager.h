d0383e6e709da3d0e96138d6a5b4d0f97415dbcb
Manuel Stampfl <091aet21@htl-kapfenberg.ac.at>
2014-02-22 00:03:24 +0100

    StateManager hinzugefügt Informationen zu den Status-Codes

---
+++ Server/Arduino/EVSE/StateManager.h
@@ -0,0 +1,38 @@
+#ifndef STATEMANAGER_H
+#define STATEMANAGER_H
+
+/*
+    Status-Codes:
+      A1  Nichts angeschlossen, 12V statisch 
+      A2  Nichts angeschlossen, 12V PWM
+    
+      B1  Angeschlossen, 9V statisch
+      B2  Angeschlossen, 9V PWM
+    
+      C1  Bereit, 6V statisch (ohne Lüfter)
+      C2  Bereit, 6V PWM (ohne Lüfter)
+    
+      D1  Bereit, 3V statisch (mit Lüfter)
+      D2  Bereit, 3V PWM (mit Lüfter)
+    
+      E  Entweder Spannungsausfall am EVSE oder Kurzschloss zwischen CP PE (0V)
+      F  EVSE nicht verfügbar (-12V statisch)
+      
+    Wie wird der Status bestimmt?
+      Das Einlesen erfolgt über Pin A0 mit einer Spannung von 0-5V (-12V bis +12V in 1024 Schritten)
+      
+    Stromstärke wird über PWM mitgeteilt
+    
+    1 -> Wir können keinen Strom liefern
+    2 -> Wir können Strom liefern
+*/
+
+
+class CStateManager
+{
+ 
+};
+
+extern CStateManager StateManager;
+
+#endif
