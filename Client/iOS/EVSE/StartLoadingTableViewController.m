c285970cb28e64090046eed3ff7f57f12874daa7
Manuel Stampfl <091aet21@htl-kapfenberg.ac.at>
2014-03-20 20:51:53 +0100

    Logos hinzugefügt Fehlermeldungen

--- Client/iOS/EVSE/StartLoadingTableViewController.m
+++ Client/iOS/EVSE/StartLoadingTableViewController.m
@@ -11,7 +11,6 @@
 
 @interface StartLoadingTableViewController ()
 @property (weak, nonatomic) IBOutlet UITableViewCell *requestLoadingCurrentCell;
-@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 @property (weak, nonatomic) IBOutlet UITableViewCell *cancelLoadingCell;
 
 @end
