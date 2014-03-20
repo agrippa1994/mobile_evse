1febc4f0373552060075b776bc0eead1bd83f617
agrippa1994 <manuel.stampfl@gmail.com>
2014-03-20 21:41:08 +0100

    Starten der Ladung, Stoppen der Ladung und Überwachung in seperate Tabs eingefügt. Grundlegende Designänderungen Stabilität erhöht

---
+++ Client/iOS/EVSE/ControllingTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ControllingTableViewController.h
+//  EVSE
+//
+//  Created by Manuel on 20.03.14.
+//  Copyright (c) 2014 Manuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ControllingTableViewController : UITableViewController
+
+@end
