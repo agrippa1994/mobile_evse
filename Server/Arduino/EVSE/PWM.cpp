7385d23c73a22b6d53615a4d05bf4bcd2cd4c3df
Manuel Stampfl <091aet21@htl-kapfenberg.ac.at>
2014-03-24 11:55:28 +0100

    PWM abgeändert.

--- Server/Arduino/EVSE/PWM.cpp
+++ Server/Arduino/EVSE/PWM.cpp
@@ -5,17 +5,6 @@ void pwm_init()
 {   
   // Pin 10 wird für die PWM verwendet
   pinMode(PIN_PWM, OUTPUT);
-  
-  // Erstellen einer Pulsweitenmodulation auf dem digitalen Pin 10
-  TCCR1A = _BV(COM1A0) | _BV(COM1B1) | _BV(WGM11) | _BV(WGM10);
-  TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS11) | _BV(CS10);
-  
-  // 0 - 249 ist das Tastverhältnis
-  // Um das Tastverhältnis der Pulsweitenmodulation zu verändern,
-  // muss das Register OCR1B von 0 - 249 gesetzt werden
-  
-  OCR1A = 249;   // 1kHz
-  OCR1B = 0;     // keine Ausgangsspannung
 }
 
 void setPWM(int value)
@@ -24,8 +13,27 @@ void setPWM(int value)
   if(value < 0 || value > 249)
     return;
     
-  // setzen des Registers  
-  OCR1B = value;
+  if(value == 0)
+  {
+    digitalWrite(PIN_PWM, LOW);
+  }
+  else if (value == 249)
+  {
+    digitalWrite(PIN_PWM, HIGH); 
+  }
+  else
+  {
+    // Erstellen einer Pulsweitenmodulation auf dem digitalen Pin 10
+    TCCR1A = _BV(COM1A0) | _BV(COM1B1) | _BV(WGM11) | _BV(WGM10);
+    TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS11) | _BV(CS10);
+  
+    // 0 - 249 ist das Tastverhältnis
+    // Um das Tastverhältnis der Pulsweitenmodulation zu verändern,
+    // muss das Register OCR1B von 0 - 249 gesetzt werden
+  
+    OCR1A = 249;   // 1kHz
+    OCR1B = value; 
+  }
 }
 
 // Gibt das Tastverhältnis der PWM zurück (0-249)
