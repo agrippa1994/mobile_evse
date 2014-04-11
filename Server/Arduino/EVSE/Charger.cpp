f6c8aaf2edfe6bfc706ddb90bf17e332a9b37b8a
agrippa1994 <manuel.stampfl@gmail.com>
2014-04-11 15:20:04 +0200

    Arduino     Weitere Dateien durch das bessere Format verbessert

--- Server/Arduino/EVSE/Charger.cpp
+++ Server/Arduino/EVSE/Charger.cpp
@@ -17,76 +17,76 @@ Timer g_stopTimer;
 // Timer-Callback welcher aufgerufen wird, falls die Ladung deaktiviert wird
 void charger_stopChargingHandler(Timer *t)
 {
-  
+
 }
 
 // Initialisieren des Ladecontrollers
 void charger_init()
 {
-    pinMode(PIN_RELAY,OUTPUT);
+	pinMode(PIN_RELAY,OUTPUT);
 
-    // Deaktivieren der Ladung
-    disableCharging();
-    disableRelay();
-  
-    g_chargingTime = 0;
+	// Deaktivieren der Ladung
+	disableCharging();
+	disableRelay();
+
+	g_chargingTime = 0;
 }
 
 // Updatedes Ladecontrollers
 void charger_update()
 {
-    g_stopTimer.update();  
+	g_stopTimer.update();  
 }
 
 // Aktivieren der PWM
 void enableCharging(int amps)
 {
-    setPWMAmpere(amps);
+	setPWMAmpere(amps);
 }
 
 // Deaktivierne der PWM
 void disableCharging()
 {
-    if(g_isLoading) // Anfrage zum Stoppen
-    {
-        g_stopTimer.start(3000, charger_stopChargingHandler, false); 
-    }
+	if(g_isLoading) // Anfrage zum Stoppen
+	{
+		g_stopTimer.start(3000, charger_stopChargingHandler, false); 
+	}
 
-    // konstante Ausgangsspannung am CP-Ausgang
-    setPWM(249);
+	// konstante Ausgangsspannung am CP-Ausgang
+	setPWM(249);
 }
 
 // Aktivieren des Relais
 void enableRelay()
 {
-    g_currentLoadingCurrent = g_requestLoadingCurrent;
-    g_chargingTime = millis();
-    g_isLoading = true;
+	g_currentLoadingCurrent = g_requestLoadingCurrent;
+	g_chargingTime = millis();
+	g_isLoading = true;
 
-    digitalWrite(PIN_RELAY, HIGH);
+	digitalWrite(PIN_RELAY, HIGH);
 }
 
 // Deaktivieren des Relais
 void disableRelay()
 {
-    g_currentLoadingCurrent = 0;
-    g_isLoading = false;
+	g_currentLoadingCurrent = 0;
+	g_isLoading = false;
 
-    digitalWrite(PIN_RELAY, LOW);
+	digitalWrite(PIN_RELAY, LOW);
 }
 
 // Aktuelle Ladezeit
 unsigned long chargingTime()
 {
-    if(!g_isLoading)
-        return 0;
+	if(!g_isLoading)
+		return 0;
 
-    return millis() - g_chargingTime;
+	return millis() - g_chargingTime;
 }
 
 // Ist Ladung aktiv?
 bool isLoading()
 {
-    return g_isLoading;
+	return g_isLoading;
 }
 
--- Server/Arduino/EVSE/EVSE.cpp
+++ Server/Arduino/EVSE/EVSE.cpp
@@ -38,173 +38,173 @@ void send_usb_timer(Timer *t);
 // Hier werden unter anderem alle Objekte initialisiert, aber nicht konstruiert
 void setup()
 {
-  // Initialisieren der Pulsweitenmodulation
-    pwm_init();
+	// Initialisieren der Pulsweitenmodulation
+	pwm_init();
 
-    // Initialisieren des Ladecontrollers
-    charger_init();
+	// Initialisieren des Ladecontrollers
+	charger_init();
 
-    // Initialisieren des State-Managers
-    statemanager_init(evseStateChange);
+	// Initialisieren des State-Managers
+	statemanager_init(evseStateChange);
 
-    // Initialisieren des USB-Kommunikation
-    usb_init(commandHandler); 
+	// Initialisieren des USB-Kommunikation
+	usb_init(commandHandler); 
 
-    // Starten des USB-Timers
-    g_usbTimer.start(g_updateSpeed, send_usb_timer, true);
+	// Starten des USB-Timers
+	g_usbTimer.start(g_updateSpeed, send_usb_timer, true);
 }
 
 // loop() wird in einer for(;;) - Schleife unendlich lange aufgerufen
 void loop()
 {
-    // Updaten des Statusmanagers und Ladecontroller
-    statemanager_update();
-    charger_update();
+	// Updaten des Statusmanagers und Ladecontroller
+	statemanager_update();
+	charger_update();
 
-    // Update des USB-Timers
-    g_usbTimer.update();  
+	// Update des USB-Timers
+	g_usbTimer.update();  
 }
 
 // Verarbeiten der Befehle, welche per USB empfangen werden
 // Mögliche Befehle
-//   startloading --current [STROM]
-//   stoploading
-//   config --pwm [PWM]
-//   config --digitalWrite [PIN] --value [0/1]
-//   config --updatespeed [SPEED in ms]
-//   config --force [Index nach eState]
-//   config --force disable
+// 		startloading --current [STROM]
+// 		stoploading
+//   	config --pwm [PWM]
+//   	config --digitalWrite [PIN] --value [0/1]
+//   	config --updatespeed [SPEED in ms]
+//   	config --force [Index nach eState]
+//   	config --force disable
 void commandHandler(const char *sz)
 {
-    if(strstr(sz, "startloading --current") != 0)
-    {
-        if(getEVSEState() != state_B)
-            return;
-      
-        int current = 0;
-        if(sscanf(sz, "startloading --current %d", &current) == 1)
-        {
-            g_requestLoading = 1;
-            g_requestStopLoading = 0; 
-            g_requestLoadingCurrent = current;
-            enableCharging(g_requestLoadingCurrent);
-        }
-        return;
-    }
+	if(strstr(sz, "startloading --current") != 0)
+	{
+		if(getEVSEState() != state_B)
+			return;
 
-    else if(strstr(sz, "stoploading") != 0)
-    {
-        if(!isLoading())
-            return;
-      
-        g_requestLoading = 0;
-        g_requestLoadingCurrent = 0;
-        disableCharging();
-        return;
-    }
+		int current = 0;
+		if(sscanf(sz, "startloading --current %d", &current) == 1)
+		{
+			g_requestLoading = 1;
+			g_requestStopLoading = 0; 
+			g_requestLoadingCurrent = current;
+			enableCharging(g_requestLoadingCurrent);
+		}
+		return;
+	}
 
-    else if(strstr(sz, "config") != 0)
-    {
-        if(strstr(sz, "config --pwm") != 0)
-        {
-            int pwm = 0;
-            if(sscanf(sz, "config --pwm %d", &pwm) == 1)
-            {
-                setPWM(pwm);
-            } 
-        }
-        else if(strstr(sz, "config --digitalWrite") != 0)
-        {
-            int pin = 0, val = 0;
-            if(sscanf(sz, "config --digitalWrite %d --value %d", &pin, &val) == 2)
-            {
-                if(pin <= 1 || pin > 13)
-                    return;
-                  
-                if(val == 0 || val == 1)
-                    digitalWrite(pin, val);
-            } 
-        }
-        else if(strstr(sz, "config --updatespeed") != 0)
-        {
-            int speed = 0;
-            if(sscanf(sz, "config --updatespeed %d", &speed) == 1)
-            {
-                if(speed >= 100)
-                    g_updateSpeed = speed;
-            } 
-        }
-        else if(strstr(sz, "config --force") != 0)
-        {
-            char param[28] = { 0 };
-            if(sscanf(sz, "config --force %s", param))
-            {
-                if(strcmp(param, "disable") == 0)
-                {
-                    g_stateForce = 0;
-                    g_stateForceState = 0;
-                    return;
-                }
+	else if(strstr(sz, "stoploading") != 0)
+	{
+		if(!isLoading())
+			return;
 
-                int idx = atoi(param);     
-                if(idx < 0 || idx > 6)
-                    return;
+		g_requestLoading = 0;
+		g_requestLoadingCurrent = 0;
+		disableCharging();
+		return;
+	}
 
-                g_stateForceState = idx;
-                g_stateForce = 1;
-            }
-        }
-    }
+	else if(strstr(sz, "config") != 0)
+	{
+		if(strstr(sz, "config --pwm") != 0)
+		{
+			int pwm = 0;
+			if(sscanf(sz, "config --pwm %d", &pwm) == 1)
+			{
+				setPWM(pwm);
+			} 
+		}
+		else if(strstr(sz, "config --digitalWrite") != 0)
+		{
+			int pin = 0, val = 0;
+			if(sscanf(sz, "config --digitalWrite %d --value %d", &pin, &val) == 2)
+			{
+				if(pin <= 1 || pin > 13)
+					return;
+
+				if(val == 0 || val == 1)
+					digitalWrite(pin, val);
+			} 
+		}
+		else if(strstr(sz, "config --updatespeed") != 0)
+		{
+			int speed = 0;
+			if(sscanf(sz, "config --updatespeed %d", &speed) == 1)
+			{
+				if(speed >= 100)
+					g_updateSpeed = speed;
+			} 
+		}
+		else if(strstr(sz, "config --force") != 0)
+		{
+			char param[28] = { 0 };
+			if(sscanf(sz, "config --force %s", param))
+			{
+				if(strcmp(param, "disable") == 0)
+				{
+					g_stateForce = 0;
+					g_stateForceState = 0;
+					return;
+				}
+
+				int idx = atoi(param);     
+				if(idx < 0 || idx > 6)
+					return;
+
+				g_stateForceState = idx;
+				g_stateForce = 1;
+			}
+		}
+	}
 }
 
 // Callback für den genormten EVSE Status
 // Wird aufgerufen, falls eine Änderung erfolgt
 void evseStateChange(eState oldState, eState newState)
 {
-    if(newState == oldState)
-        return;
+	if(newState == oldState)
+		return;
 
-    // Ausfuehren aller Sequenzen laut Norm!
-    if( (((oldState == state_C) || (newState == state_D)) && ((newState == state_A) || (newState == state_B)))
-    ||( (newState == state_A || newState == state_E || newState == state_F)))
-    {
-        disableCharging();
-        disableRelay();
-    }
+	// Ausfuehren aller Sequenzen laut Norm!
+	if( (((oldState == state_C) || (newState == state_D)) && ((newState == state_A) || (newState == state_B)))
+	||( (newState == state_A || newState == state_E || newState == state_F)))
+	{
+		disableCharging();
+		disableRelay();
+	}
 
-    else if((oldState == state_B) && (newState == state_C || newState == state_D) && g_requestLoading)
-    {
-        enableCharging(g_requestLoadingCurrent);
-        enableRelay();
+	else if((oldState == state_B) && (newState == state_C || newState == state_D) && g_requestLoading)
+	{
+		enableCharging(g_requestLoadingCurrent);
+		enableRelay();
 
-        g_requestLoading = 0;
-        g_requestLoadingCurrent = 0;
-    }
+		g_requestLoading = 0;
+		g_requestLoadingCurrent = 0;
+	}
 }
 
 void send_usb_timer(Timer *p)
-{      
+{
 
-  // Timer-Intervall auf die Update-Geschwindigkeit setzen
-    p->setTimerInterval(g_updateSpeed);
+	// Timer-Intervall auf die Update-Geschwindigkeit setzen
+	p->setTimerInterval(g_updateSpeed);
 
-    // Senden jener Daten, die für die Visualisierungen wichtig sind
-    String data;
-    addValueToString(data, "state", getEVSEState(), true);
-    addValueToString(data, "requestLoading", g_requestLoading);
-    addValueToString(data, "requestLoadingCurrent", g_requestLoadingCurrent);
-    addValueToString(data, "requestStopLoading", g_requestStopLoading);
-    addValueToString(data, "isLoading", isLoading());
-    addValueToString(data, "updateSpeed", g_updateSpeed);
-    addValueToString(data, "PWM", getPWM());
-    addValueToString(data, "temperature", (double)(analogRead(PIN_TEMPERATURE) * 0.49));
-    addValueToString(data, "chargingTime", chargingTime());
-    addValueToString(data, "force", g_stateForce);
-    addValueToString(data, "currentLoadingCurrent", g_currentLoadingCurrent);
+	// Senden jener Daten, die für die Visualisierungen wichtig sind
+	String data;
+	addValueToString(data, "state", getEVSEState(), true);
+	addValueToString(data, "requestLoading", g_requestLoading);
+	addValueToString(data, "requestLoadingCurrent", g_requestLoadingCurrent);
+	addValueToString(data, "requestStopLoading", g_requestStopLoading);
+	addValueToString(data, "isLoading", isLoading());
+	addValueToString(data, "updateSpeed", g_updateSpeed);
+	addValueToString(data, "PWM", getPWM());
+	addValueToString(data, "temperature", (double)(analogRead(PIN_TEMPERATURE) * 0.49));
+	addValueToString(data, "chargingTime", chargingTime());
+	addValueToString(data, "force", g_stateForce);
+	addValueToString(data, "currentLoadingCurrent", g_currentLoadingCurrent);
 
-    // Pin-Daten der analogen Pins
-    for(int i=A0, u = 0; i <= A5; i ++, u++)
-        addValueToString(data, String("A") + String(u), analogRead(i));
+	// Pin-Daten der analogen Pins
+	for(int i=A0, u = 0; i <= A5; i ++, u++)
+		addValueToString(data, String("A") + String(u), analogRead(i));
 
-    Serial.println(data);
+	Serial.println(data);
 }
